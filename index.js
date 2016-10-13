0860229117959cc52394ee3e0625e5f44a0f21c8
Mike Meadows <mmeadow33@gmail.com>
2016-10-13 11:10:19 -0500

    more solutions for sqlite queries

--- index.js
+++ index.js
@@ -116,14 +116,36 @@ const knex = require('knex')({
 // console.log('6. Provide a query showing the invoices of customers who are from Brazil.')
 // knex('Invoice').where('BillingCountry', 'Brazil').then(console.log)
 
-console.log(`7. Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.`)
+// console.log(`7. Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.`)
+// knex('Invoice')
+//     .select(knex.raw(`
+//     Employee.FirstName || ' ' || Employee.LastName as SalesAgent,
+//     Invoice.*
+//   `))
+//     .join('Customer', 'Invoice.CustomerId', 'Customer.CustomerId')
+//     .join('Employee', 'Customer.SupportRepId', 'Employee.EmployeeId')
+//     .then(console.log)
+
+// console.log(`8. Provide a query that shows the Invoice Total, Customer name, Country and Full Sales Agent name for all invoices and customers.`)
+// knex("Invoice")
+//     .select(knex.raw('Invoice.Total, Customer.FirstName, Customer.Country, Employee.LastName'))
+//     .join('Customer', 'Invoice.CustomerId', 'Customer.CustomerId')
+//     .join('Employee', 'Customer.SupportRepId', 'Employee.EmployeeId')
+//     .then(console.log)
+
+
+/////////Prettier version of my answer/////////////
+console.log(`8. Provide a query that shows the Invoice Total, Customer name, Country and Full Sales Agent name for all invoices and customers.`)
 knex('Invoice')
-  .select(knex.raw(`
-    Employee.FirstName || ' ' || Employee.LastName as SalesAgent,
-    Invoice.*
-  `))
-  .join('Customer', 'Invoice.CustomerId', 'Customer.CustomerId')
-  .join('Employee', 'Customer.SupportRepId', 'Employee.EmployeeId')
-  .then(console.log)
+    .select(knex.raw(`Employee.FirstName || ' ' || Employee.LastName as SalesAgent`))
+    .select(knex.raw(`Customer.FirstName || ' ' || Customer.LastName as Customer`))
+    .select('Customer.Country')
+    .sum('Invoice.Total as Total')
+    .join('Customer', 'Invoice.CustomerId', 'Customer.CustomerId')
+    .join('Employee', 'SupportRepId', 'EmployeeId')
+    .groupBy('Customer.CustomerId')
+    .orderBy('Total', 'desc')
+    .then(console.log)
+
 
 knex.destroy()
